947248b80e7243a05dcea335d7c14ee51e60bbf0
Danny McMurrough <mcmurrough1dan@gmail.com>
2017-04-07 04:02:02 +0000

    update routes

---
+++ app.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT; //Use for could9 autoassign port
+
+app.use(express.static('public'));  //Look first in static content
+app.use(express.static('src/views'));  //Second static dir 
+app.use(express.static('bower_components'));
+
+
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+   //res.send(index.html); 
+   res.render('index', { 
+       list: ['frist val', '2nd val', '3rd val'], 
+       nav: [
+            {Link: 'Services', Text: 'Services'},
+            {Link: 'Portfolio', Text: 'Portfolio'},
+            {Link: 'About', Text: 'About'},
+            {Link: 'Team', Text: 'Team'},
+            {Link: 'Contact', Text: 'Contact'}]
+   });
+});
+
+app.get('/mochi', function(req, res){
+   res.send("Lovvin it."); 
+});
+
+app.listen(port, function(err){
+    console.log("The server is running on port: " + port);
+});
+
